88bbd412cead8ebc30b7be615999972e9202c6f2
KarimMartin1998 <102755385+KarimMartin1998@users.noreply.github.com>
2023-08-08 18:50:07 +0100

    Add files via upload

--- TurtleRacingGame.py
+++ TurtleRacingGame.py
@@ -1,32 +1,32 @@
-import turtle
+#import turtle
 from turtle import Turtle, Screen
 import random
 
-is_race_on = False
+is_race_on = False # Sets up the game loop
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? (Enter colour)")
-colours = ["red", "green", "yellow", "orange", "blue", "purple"]
-all_turtles = []
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter: red, green, yellow, orange, blue or purple")
+colours = ["red", "green", "yellow", "orange", "blue", "purple"] #list of colours
+all_turtles = [] # List of turtle objects
 
 
 y_axis = -70
 for colour in colours:
-    new_turtle = Turtle(shape="turtle")
-    new_turtle.color(colour)
-    new_turtle.penup()
-    new_turtle.goto(x=-230, y=y_axis)
-    y_axis = y_axis + 30
-    all_turtles.append(new_turtle)
+    new_turtle = Turtle(shape="turtle") # Create a new turtle for each colour
+    new_turtle.color(colour) # Change colour to each one in the list
+    new_turtle.penup() # Lift pen prior to moving their position
+    new_turtle.goto(x=-230, y=y_axis) # Set new position along X-axis and Y-axis
+    y_axis = y_axis + 30 #Add 30 to Y-axis variable to move each turtle upwards
+    all_turtles.append(new_turtle) # Add turtle objects to the list
 
-if user_bet:
-    is_race_on = True
+if user_bet: # Checks the user has input a colour
+    is_race_on = True # Begins game loop is user has made an input
 
-while is_race_on:
-    for turtle in all_turtles:
-        if turtle.xcor() > 230:
-            is_race_on = False
-            winning_colour = turtle.pencolor()
+while is_race_on: # While there is a user input:
+    for turtle in all_turtles: # Loop through turtle objects in all_turtles list
+        if turtle.xcor() > 230: # If one of the turtles X-cord is over 230:
+            is_race_on = False  # Stop the loop
+            winning_colour = turtle.pencolor() # Save the winning turtle in winning_colour variable
             if winning_colour == user_bet:
                 print(f"You've won! The {winning_colour} is the winner")
             else:
@@ -34,7 +34,7 @@ while is_race_on:
 
 
         random_distance = random.randint(0, 10)
-        turtle.forward(random_distance)
+        turtle.forward(random_distance) # Move all turtles forward a random distance between 0 and 10
 
 
 
